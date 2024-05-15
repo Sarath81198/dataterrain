cf847b9ca3ac40eed6351ad81f8194f85b0cf874
Sarath Dev <sarathdev@Saraths-MacBook-Pro.local>
2024-05-15 10:40:49 +0530

    feat: top nav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,7 +4,12 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
-import './styles/TopNav.css'
+import './styles/TopNav.css';
+import logo from '../assets/images/logo.png';
+import dayMode from '../assets/icons/theme.png';
+import notifications from '../assets/icons/notifications.png';
+import settings from '../assets/icons/settings.png';
+import chat from '../assets/icons/chat.png';
 
 function TopNav() {
   return (
@@ -12,9 +17,15 @@ function TopNav() {
       <AppBar position="static" color="transparent" className='top-nav'>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            <img src={logo} alt="Logo" className="logo" />
           </Typography>
-          <Avatar>N</Avatar>
+          <div className="top-nav-icon-container">
+            <img alt='Daymode' src={dayMode} className='top-nav-icons' />
+            <img alt='Chat' src={chat} className='top-nav-icons' />
+            <img alt='Notifications' src={notifications} className='top-nav-icons' />
+            <img alt='Settings' src={settings} className='top-nav-icons' />
+            <Avatar alt="User" className='top-nav-avatar' />
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
